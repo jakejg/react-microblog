5c8e21363348221fa228fc4f6fbd9a30cb46b1c9
Jake <jakefrisbee@gmail.com>
2020-06-21 08:55:33 -0700

    preloaded a post into app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import {v4 as uuid} from 'uuid';
 
 function App() {
-    const [posts, setPosts] = useState({});
+    const [posts, setPosts] = useState({12:{title: "Test", description: "This is a test post", body: "Test this thing out!"}});
 
     const addPost = ({title, description, body}) => {
         setPosts(posts => ({...posts, [uuid()]: {title, description, body}}))
@@ -17,5 +17,7 @@ function App() {
         </div>
     );
 }
+
+
 
 export default App;
